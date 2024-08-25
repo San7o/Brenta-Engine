6ee90e46a6f255fcade04cf8bc331a1a3c799eb1
San7o <santigio2003@gmail.com>
2024-08-25 13:02:08 +0200

    release now generates tar files with the engine name

--- release.sh
+++ release.sh
@@ -12,5 +12,5 @@ rm -rf release
 mkdir -p release
 
 # Copy the files to the release directory
-tar -czf release/docs-v$1.tar.gz docs/html
-tar -czf release/src-v$1.tar.gz engine utils ecs tests examples CMakeLists.txt README.md LICENSE flake.nix flake.lock lib Makefile include
+tar -czf release/brenta-engine-docs-v$1.tar.gz docs/html
+tar -czf release/brenta-engine-src-v$1.tar.gz engine utils ecs tests examples CMakeLists.txt README.md LICENSE flake.nix flake.lock lib Makefile include
