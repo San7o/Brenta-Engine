2596f435f022543685297eb0a27a7a699e1d423e
San7o <santigio2003@gmail.com>
2025-12-09 23:59:01 +0100

    examples: implemented shadertoy

--- examples/shadertoy.cpp
+++ examples/shadertoy.cpp
@@ -12,6 +12,7 @@
 #ifdef BRENTA_USE_IMGUI
 
 #include <brenta/engine.hpp>
+#include <imgui_stdlib.h>
 
 #include <iostream>
 
@@ -19,6 +20,8 @@
 REGISTER_SYSTEMS()
 
 using namespace brenta;
+
+unsigned int update_shader(std::string fragment_source);
 
 int main(int argc, char** argv)
 {
@@ -40,8 +43,8 @@ int main(int argc, char** argv)
                .cull_face()
                .multisample()
                .depth_test())
-    .subsystem(gui::builder())
     .subsystem(input::builder())
+    .subsystem(gui::builder())
     .build();
   
   auto ret = engine.initialize();
@@ -61,18 +64,58 @@ int main(int argc, char** argv)
   // Game loop
   //
 
+    // A square
+  float vertices[] = {// First Triangle
+                      -1.0f, 1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+
+                      // Second Triangle
+                      -1.0f, 1.0f, 0.0f, -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f};
+  types::vao v;
+  v.init();
+  types::buffer vbo = types::buffer(GL_ARRAY_BUFFER);
+  vbo.copy_data(sizeof(vertices), vertices, GL_STATIC_DRAW);
+  v.set_vertex_data(vbo, 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+  std::string fragment_source = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "in vec3 ourColor;\n\n"
+    "void main()\n"
+    "{\n"
+    "  FragColor = vec4(ourColor, 1.0);\n"
+    "}\0";
+  std::string prev_source = fragment_source;
+  unsigned int shader_program = update_shader(fragment_source);
+  
   while(!window::should_close())
   {
     if (window::is_key_pressed(GLFW_KEY_ESCAPE))
       window::close();
 
+    if (prev_source != fragment_source)
+    {
+      prev_source = fragment_source;
+      unsigned int ret = update_shader(fragment_source);
+      if (ret != 0)
+        shader_program = ret;
+    }
+    
     gui::new_frame(&fb);
 
+    ImGui::Begin("Shader source");
+    ImGui::InputTextMultiline("##source", &fragment_source,
+                              ImVec2(1000, 1000),
+                              ImGuiInputTextFlags_AllowTabInput);
+    ImGui::End();
+
     fb.bind();
+    
     gl::clear();
+    v.bind();
 
-    // Draw here....
-
+    glUseProgram(shader_program);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    v.unbind();
     fb.unbind();
     gui::render();
     
@@ -91,6 +134,65 @@ int main(int argc, char** argv)
     return 1;
   }
   return 0;
+}
+
+unsigned int update_shader(std::string fragment_source)
+{
+
+  const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   ourColor = aColor;\n"
+    "}\0";
+  unsigned int vertexShader;
+  int success;
+  char infoLog[512];
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    fprintf(stderr, "Vertex shader compilation failed: %s\n", infoLog);
+    return 0;
+  }
+
+  const char *fragmentShaderSource = fragment_source.c_str();
+  unsigned int fragmentShader;
+  success = 1;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    fprintf(stderr, "Fragment shader compilation failed: %s\n", infoLog);
+    return 0;
+  }
+
+  success = 1;
+  unsigned int shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    return 0;
+  }
+
+  glUseProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+  
+  return shaderProgram;
 }
 
 #endif // BRENTA_USE_IMGUI
